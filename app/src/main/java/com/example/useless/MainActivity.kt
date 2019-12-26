108665188655dff930284ae9301fe12644cf03ee
chonsean98 <khuehcs-pa16@student.tarc.edu.my>
2019-12-26 16:03:31 +0800

    initial

--- app/src/main/java/com/example/useless/MainActivity.kt
+++ app/src/main/java/com/example/useless/MainActivity.kt
@@ -1,24 +1,44 @@
 package com.example.useless
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var usefulViewModel: UsefulViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        //Initialize a RecyclerView adapter
+
+        //Initialize the ViewModel
+
         fab.setOnClickListener { view ->
+            val intent = Intent(this, AddActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                //TODO : Save a useful record to the database
+                Toast.makeText(this, "Record saved", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,5 +55,9 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    companion object{
+        const val REQUEST_CODE = 1
     }
 }
